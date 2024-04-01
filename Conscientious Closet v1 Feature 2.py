02957545103b49d11336d8309e96900508789e6f
adriancliang <165630208+adriancliang@users.noreply.github.com>
2024-03-31 22:15:09 -0700

    Add files via upload

---
+++ Conscientious Closet v1 Feature 2.py
@@ -0,0 +1,35 @@
+import os
+import openai
+import streamlit as st
+from openai import OpenAI
+
+
+st.markdown("# Prototype Feature 2: Suggestions Based on Weather")
+st.sidebar.markdown("# Prototype Feature 2: Suggestions Based on Weather")
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+client = OpenAI()
+
+
+# create a wrapper function
+def get_completion(prompt, model="gpt-3.5-turbo"):
+   completion = client.chat.completions.create(
+        model=model,
+        messages=[
+        {"role":"system",
+         "content": "Based on the location provided, display a weather forecast for today and the rest of the week and provide suggestions on clothing to wear based on today's weather."},
+        {"role": "user",
+         "content": prompt},
+        ]
+    )
+   return completion.choices[0].message.content
+
+# create our streamlit app
+with st.form(key = "chat"):
+    prompt = st.text_input("What is your current location? ") 
+    
+    submitted = st.form_submit_button("Submit")
+    
+    if submitted:
+        st.write(get_completion(prompt))
